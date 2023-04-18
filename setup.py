e95d1b37ab04891c2ee78b6c8b24b0fd5224c5e1
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-18 02:38:37 -0700

    updated description

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='gitfluencer',
     version='0.1.4',
     description='Utility tool for interacting with the GitHub platform',
     author='jjoeldaniel',
     author_email='joeldanielrico@gmail.com',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     url='https://github.com/jjoeldaniel/gitfluencer',
+    keywords=['github', 'social'],
     packages=find_packages(),
     install_requires=[
         'PyGithub',
