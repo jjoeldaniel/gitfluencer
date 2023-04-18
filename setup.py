4261819cc2d6857f59210454d75fba5064b5a194
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-18 01:27:49 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gitfluencer',
-    version='0.1.0',
+    version='0.1.1',
     description='Utility tool for interacting with the GitHub platform',
     author='jjoeldaniel',
     author_email='joeldanielrico@gmail.com',
